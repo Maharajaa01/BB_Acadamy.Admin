8e835b56db140043ea24b16bb5a664b7b7482df6
Maharaja <vijaymaharajan38@gmail.com>
2026-01-13 22:36:16 +0530

    FIx:Admission Form

---
+++ bb_acadamy_admin/black_building_admin/doctype/admission_inquiry/admission_inquiry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Maha Raja and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Admission Inquiry", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ bb_acadamy_admin/black_building_admin/doctype/study_material/study_material.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Maha Raja and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Study Material", {
+// 	refresh(frm) {
+
+// 	},
+// });
